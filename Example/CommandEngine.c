60d68e1bff8673c52b6d635bd69f4a2817193b21
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2015-01-09 11:18:35 +0100

    Starting the examples on how to use the commands core

---
+++ Example/CommandEngine.c
@@ -0,0 +1,156 @@
+
+#include "CommandEngine.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Module definitions
+////////////////////////////////////////////////////////////////////////////////
+
+#define MAX_BUFFER 0xFF
+#define NULL (0L)
+
+byte CommandBuffer[MAX_BUFFER];
+
+////////////////////////////////////////////////////////////////////////////////
+// Example command implementation
+//      Commands run on demand from terminal. They can have variable arguments,
+//      like any terminal command.
+//
+//      Usage: example-command [???]
+////////////////////////////////////////////////////////////////////////////////
+byte* ExampleImplementation(const char* args[], struct CommandEngine* commandEngine)
+{
+    // TODO: interact with the service
+    return CMD_CRLF "Done." CMD_CRLF;
+}
+
+const Command ExampleCommand = {
+    "example-command",
+    ExampleImplementation
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Example process implementation
+//      Processes are the same as commands, the only difference is that
+//      they keep the focus from the command engine. No commands can run
+//      when a process is running, however services keep looping on the
+//      background. You can use CTRL+C to close a process
+//
+//      Usage: example.exe [???]
+////////////////////////////////////////////////////////////////////////////////
+
+void ExampleAppImplementation(const char key, struct CommandEngine* commandEngine)
+{
+    // TODO: Use buttons to orchestrate services
+}
+
+void ExampleAppLoadImplementation(const char* args[], struct CommandEngine* commandEngine)
+{
+    commandEngine->WriteToOutput(
+    CMD_CRLF "Example process"
+    // TODO: add description about the buttons
+    CMD_CRLF);
+}
+
+void ExampleAppCloseImplementation(struct CommandEngine* commandEngine)
+{
+    commandEngine->WriteToOutput("\r\n[Ended]\r\n");
+}
+
+const Application ControlApplication = {
+    "example.exe",
+    "Example process",
+    ExampleAppImplementation,
+    ExampleAppLoadImplementation,
+    ExampleAppCloseImplementation
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Example background services
+////////////////////////////////////////////////////////////////////////////////
+
+byte ServiceExample1Implementation(byte state, struct CommandEngine* commandEngine)
+{
+    // Do something depending the state
+    switch(state)
+    {
+        case Starting:
+            return 0x01;
+        case 0x01:
+            // TODO: Loops and processes some input
+            return 0x01;
+        default:
+            break;
+    }
+
+    return Stopped;
+}
+
+Service Example1Service = {
+    "Example service 1",
+    "Example background service that runs forever",
+    ServiceExample1Implementation,
+};
+
+byte ServiceExample2Implementation(byte state, struct CommandEngine* commandEngine)
+{
+    // Do something depending the state
+    switch(state)
+    {
+        case Starting:
+            return 0x01;
+        case 0x01:
+            // TODO: Step 1
+            return 0x02;
+        case 0x02:
+        default:
+            // TODO: Step 2 and stopping
+            break;
+    }
+
+    return Stopped;
+}
+
+Service Example2Service = {
+    "Example service 2",
+    "Example background service that runs on demand",
+    ServiceExample2Implementation,
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Command engine definition
+////////////////////////////////////////////////////////////////////////////////
+const Command* Commands[] = {
+    &HelpCommand,
+    &ClearCommand,
+    NULL
+};
+
+const Application* Applications[] = {
+    &ControlApplication,
+    NULL
+};
+
+Service* Services[] = {
+    &Example1Service,
+    &Example2Service,
+    NULL
+};
+
+void DefaultErrorToOutput(const char *string) {
+    WriteString(CMD_MAKEYELLOW);
+    WriteString(CMD_MAKEBOLD);
+    WriteString(CMD_CRLF);
+    WriteString(string);
+    WriteString(CMD_CLEARATTRIBUTES);
+};
+
+CommandEngine CurrentCommandEngine = {
+    CommandBuffer,
+    MAX_BUFFER,
+    Commands,
+    Applications,
+    Services,
+    (WriterMethodType)WriteString,
+    (WriterMethodType)DefaultErrorToOutput,
+    "$> ",
+};
